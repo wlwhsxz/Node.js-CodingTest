716ff243498af68049bec85dedbde3e498b1a5c5
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-03-16 23:28:17 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,11 +1,3 @@
 function solution(numbers, k) {
-    var answer = 0;
-    let arr = [];
-    for(let i=0; i<k; i++){
-        arr.push(numbers);
-    }
-    arr = arr.toString().split(',');
-    answer = Number(arr[2*(k-1)]);
-    
-    return answer;
+    return numbers[(--k*2)%numbers.length];
 }
