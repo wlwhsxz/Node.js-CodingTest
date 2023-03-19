5d56a608f7cb6db376be89b12610126be095336f
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-03-19 23:04:06 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.98 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,16 +1,11 @@
 function solution(numbers) {
-    num = {
-        'one': 1,
-        'two': 2,
-        'three': 3,
-        'four': 4,
-        'five': 5,
-        'six': 6,
-        'seven': 7,
-        'eight': 8,
-        'nine': 9,
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
     };
-for(let i of num){
-    console.log(i)
-}
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => obj[v]);
+    console.log(num);
+
+    return Number(num);
 }
