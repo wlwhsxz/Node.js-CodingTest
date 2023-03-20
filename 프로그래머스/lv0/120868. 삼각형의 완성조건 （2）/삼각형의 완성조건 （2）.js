cb944a454ea875b24a6e9423f30904c6364694c1
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-03-21 05:45:34 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 4.16 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -0,0 +1,22 @@
+function solution(sides) {
+    var answer = 0;
+    let n = 1;
+    sides.push(n);
+    let [a,b,c] = sides
+    
+    let bool = true;
+    
+    while(bool){
+    console.log([a,b,c])
+        let max = Math.max(...sides)
+        let arr = [];
+        if( a === max && (b+c) > a) answer++;
+        else if(b === max && (a+c) > b) answer++;  
+        else if(c === max && (a+b) > c) answer++;
+        
+        c++;
+        
+        c >= a+b ? bool = false : bool;
+    }
+    return answer;
+}
