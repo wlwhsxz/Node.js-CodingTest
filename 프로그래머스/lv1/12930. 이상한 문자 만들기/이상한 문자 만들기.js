404d02c47cfd367140395ada6aa056af35129541
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-04-01 21:03:01 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  return s
+    .split(" ")
+    .map((x) =>
+      x
+        .split("")
+        .map((y, i) => (i % 2 == 0 ? y.toUpperCase() : y.toLowerCase()))
+        .join("")
+    )
+    .join(" ");
+}
