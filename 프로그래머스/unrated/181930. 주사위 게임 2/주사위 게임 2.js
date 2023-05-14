57427e22ec9cff3bee85632cbed7d71a9a48c5d4
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-05-14 20:10:08 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,8 @@
+function solution(a, b, c) {
+    var answer = 0;
+    if(a !== b && a !== c && b !== c) return a+b+c
+    if(a === b && b === c) return (a+b+c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    if(a === b || b === c || a === c) return (a+b+c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2) )
+    
+    return answer;
+}
