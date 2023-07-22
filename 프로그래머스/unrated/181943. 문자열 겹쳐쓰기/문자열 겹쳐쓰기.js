6477ba97195043e0485ba2c758ae094d97a2c608
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2023-07-22 22:45:30 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,9 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    answer += my_string.slice(0, s);
+    answer += overwrite_string;
+    answer += my_string.slice(answer.length, )
+    
+    
+    return answer;
+}
