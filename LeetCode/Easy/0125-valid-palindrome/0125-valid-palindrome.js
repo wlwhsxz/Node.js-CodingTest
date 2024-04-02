484fa2995280a0b7dce287d580bb031fb83110ba
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2024-04-02 13:06:08 +0900

    Time: 73 ms (18.94%), Space: 53.4 MB (42.31%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,7 +6,5 @@ var isPalindrome = function(s) {
     let filteredS = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     let reversedS = filteredS.split('').reverse().join('');
 
-    console.log(filteredS)
-
-    return filteredS === reversedS ? true : false
+    return filteredS === reversedS;
 };
