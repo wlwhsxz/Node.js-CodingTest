d742c30ff0efc91c29c056cabccefd1bbd68957f
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2024-04-16 14:22:46 +0900

    Time: 162 ms (9.35%), Space: 49.4 MB (77.41%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.js
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.js
@@ -4,15 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let mp = new Map()
-    
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        
-        if (mp.has(diff)) {
-            return [i, mp.get(diff)]
+    for(let i=0; i<=nums.length-1; i++) {
+        for( let j=i+1; j<=nums.length; j++) {
+            if(nums[i] + nums[j] === target) return [i, j]
         }
-        
-        mp.set(nums[i], i)
     }
 };
