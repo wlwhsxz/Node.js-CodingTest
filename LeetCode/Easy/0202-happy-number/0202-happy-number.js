30ffd01192468d224ec81eb4b7e9604c3349d18b
MINU LEE <49461207+wlwhsxz@users.noreply.github.com>
2024-04-17 18:28:02 +0900

    Time: 52 ms (84.01%), Space: 48.7 MB (98.19%) - LeetHub

---
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.js
@@ -0,0 +1,32 @@
+const squares = {'0':0, '1':1, '2':4, '3':9, '4':16, '5':25, '6':36, '7':49, '8':64, '9':81}
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    /**
+	 * Not necessary I think, but the OJ algorithm appears
+	 * to return false for non-positive numbers also.
+	 */
+    if (n < 1) return false;
+	
+	/**
+	 * This set will store numbers as we create them.
+	 * If we create a number already in the set, we
+	 * assume we're in an infinite cycle.
+	 */
+    let set = new Set();
+	
+    while (!set.has(n)) {
+        set.add(n);
+        let s = n.toString();
+        n = 0;
+        for (let i = 0; i < s.length; ++i) {
+            n += squares[s[i]];
+        }
+        if (n == 1) return true;
+    }
+	
+    return false;
+};
